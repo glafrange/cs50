bd7cfd2b6f52f6973f702d78910aff37907f77b6
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-10-25 18:22:58 -0400

    pset6 complete

--- pset6/similarities/helpers.py
+++ pset6/similarities/helpers.py
@@ -1,36 +1,53 @@
 from nltk.tokenize import sent_tokenize
 
+
 def lines(a, b):
     similar_lines = []
+    # split lines into array
     a = a.split('\n')
     b = b.split('\n')
 
-    print(a)
+    # check if sentence is similar and not a duplicate
+    for _, sentence in enumerate(a):
+        if sentence in b and sentence not in similar_lines:
+            similar_lines.append(sentence)
 
-    for i in range(len(a)):
-        if a[i] == b[i]:
-            similar_lines.append(a[i])
-
-    print(similar_lines)
     return similar_lines
 
 
 def sentences(a, b):
+    # sent_tokenize breaks strings into sentences
     a_sentences = sent_tokenize(a)
     b_sentences = sent_tokenize(b)
     similar_sentences = []
 
-    for i in range(len(a_sentences)):
-        if a_sentences[i] == b_sentences[i]:
-            similar_sentences.append(a_sentences[i])
+    # check if sentence is similar and not duplicate
+    for _, sentence in enumerate(a_sentences):
+        if sentence in b_sentences and sentence not in similar_sentences:
+            similar_sentences.append(sentence)
 
-    print(similar_sentences)
     return similar_sentences
 
 
 def substrings(a, b, n):
+    similar_substr = []
+    a_substrings = []
+    b_substrings = []
 
-    return []
+    # break strings into substrings
+    for i in range(0, len(a) - n + 1):
+        a_substrings.append(a[i:i+n])
 
+    for i in range(0, len(b) - n + 1):
+        b_substrings.append(b[i:i+n])
 
-substrings("abc. bde", "abc. def", 3)
+    # check if substrings are similar and not duplicate
+    for _, substr in enumerate(a_substrings):
+        if substr in b_substrings and substr not in similar_substr:
+            similar_substr.append(substr)
+
+    for _, substr in enumerate(b_substrings):
+        if substr in a_substrings and substr not in similar_substr:
+            similar_substr.append(substr)
+
+    return similar_substr
